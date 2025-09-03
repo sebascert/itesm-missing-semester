1eaef47ccbd0ddfcb93cf9fa87b762802bed0e14
Sebastian Certuche <sebascertuche@gmail.com>
2025-09-03 09:50:32 -0600

    feat: add formatters, ruff and prettier in dprint and clang-format

--- devtools/check.sh
+++ devtools/check.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+set -e
+
 # cd into root dir
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 cd ..
 
+# formatting
 dprint check
 
+clang_format_sources=$(find . -name '*.cpp' -o -name '*.hpp')
+if [ -n "$clang_format_sources" ]; then
+    clang-format --dry-run --Werror $clang_format_sources
+fi
+
+# markdown links
 lychee .
--- devtools/format.sh
+++ devtools/format.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
 # cd into root dir
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 cd ..
 
 dprint fmt
+
+clang_format_sources=$(find . -name '*.cpp' -o -name '*.hpp')
+if [ -n "$clang_format_sources" ]; then
+    clang-format -i $clang_format_sources
+fi
